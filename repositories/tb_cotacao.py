7fd6fdab1e8a2a859b30dfd12e62d9cbb5a7d3cc
Gabriel <gabriel.pereira2609@gmail.com>
2024-08-22 19:35:45 -0300

    feat: impl. de repository da tabela tb_cotacao

---
+++ repositories/tb_cotacao.py
@@ -0,0 +1,22 @@
+from database.db_connection import DBConnection
+import pandas as pd 
+
+SCHEMA = "public"
+TABLE = "tb_cotacao"
+
+def retornar_cotacao(db: DBConnection, data_inicio: str, data_fim: str) -> pd.DataFrame:
+    """
+    Retorna um conjunto de dados de cotação que esteja entre os valores de corte estabelecidos
+    """
+
+    query = f"""
+    SELECT vl_cotacao, dt_cotacao FROM {SCHEMA}.{TABLE}
+    WHERE dt_cotacao BETWEEN '%s' AND '%s' ORDER BY 2;
+    """ % (data_inicio, data_fim)
+
+    print(query)
+
+    with db.connection() as conn:
+        df = pd.read_sql(sql=query, con=conn)
+
+    return df
