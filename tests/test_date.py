a010091db48fd2558b88f474e643c70280c694ca
Gabriel <gabriel.pereira2609@gmail.com>
2024-09-10 18:20:57 -0300

    feat: novo teste de contagem de dias

--- tests/test_date.py
+++ tests/test_date.py
@@ -15,6 +15,12 @@ def test_retornar_n_dias_onde_datas_sao_iguais():
 
     assert retornar_n_dias(dt_inicial, dt_final) == 0
 
+def test_retornar_n_dias_onde_dias_maior_que_365():
+    dt_inicial = date(2023, 1, 1)
+    dt_final = date(2024, 1, 10)
+
+    assert retornar_n_dias(dt_inicial, dt_final) > 365
+
 def test_verificar_ordem_das_datas_nao_deve_inverter():
     dt_inicial = date(2023, 2, 1)
     dt_final = date(2023, 2, 3)
